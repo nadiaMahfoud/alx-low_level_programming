1ad9ae448115b82e65c0e62bdbee425ae08f28d0
Nadia <nadiamahfoud48@gmail.com>
2023-04-25 17:06:09 -0700

    second try

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,23 @@
-#include"main.h"
-
+#include "main.h"
 /**
- * _isalpha - function that check if alphabet
- * @c: the int cheker
- * Return: 1 if c is alpha and 0 if no
+ * _isalpha - checks for alphabetic character
+ * @c: c is an ascii character
+ *
+ * Return: 1 (if letter)
  */
+
 int _isalpha(int c)
 {
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-                return (1);
-        return (0);
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,26 @@
-#include"main.h"
-
+#include "main.h"
 /**
- * print_sign - function that check signe
- *@n: the number check
- *Return: 0 or 1 or -1
+ * print_sign - prints the sign of a number
+ * @n: n is an integer
+ *
+ * Return: 1 (if positive), 0 (if zero), -1 (if negative)
  */
+
 int print_sign(int n)
 {
-        if (n > 0)
-        {
-                _putchar('+');
-                return (1);
-        }
-        else if (n == 0)
-        {
-                _putchar('0');
-                return (0);
-        }
-        else if (n < 0)
-        {
-                _putchar('-');
-        }
-        return (-1);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
