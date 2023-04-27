60e21e27fd1cb9783bd0e3028026c7bb2380d47b
Nadia <nadiamahfoud48@gmail.com>
2023-04-27 15:17:55 -0700

    nadia

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,36 +10,41 @@
 
 int largest_number(int a, int b, int c)
 {
-        int largest;
+	int largest;
 
-        if (a > b && a > c)
-        {
-                largest = a;
-        }
-        else if (b > a && b > c)
-        {
-                largest = b;
-        }
-        else if (c > a && c > b)
-        {
-                largest = b;
-        }
-        else if (a == b)
-        {
-                if (a > c)
-                        largest = a;
-                else
-                        largest = c;
-        }
-	
-                                                                                         
-        else if (b == c)
-        {
-                if (b > a)
-                        largest = b;
-                else
-                        largest = a;
-        }
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > a && b > c)
+	{
+		largest = b;
+	}
+	else if (c > a && c > b)
+	{
+		largest = b;
+	}
+	else if (a == b)
+	{
+		if (a > c)
+			largest = a;
+		else
+			largest = c;
+	}
+	else if (a == c)
+	{
+		if (a > b)
+			largest = a;
+		else
+			largest = b;
+	}
+	else if (b == c)
+	{
+		if (b > a)
+			largest = b;
+		else
+			largest = a;
+	}
 
-        return (largest);
+	return (largest);
 }
