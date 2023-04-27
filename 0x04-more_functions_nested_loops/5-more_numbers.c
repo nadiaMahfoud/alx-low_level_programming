c4381c0cd9f04ac3fa6eb987caf5fa2ca81139ae
Nadia <nadiamahfoud48@gmail.com>
2023-04-27 06:57:06 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+        int i, j;
+
+        for (i = 0; i < 10; i++)
+        {
+                for (j = 0; j < 15; j++)
+                {
+                        if (j >= 10)
+                                _putchar(j / 10 + '0');
+                        _putchar(j % 10 + '0');
+                }
+                _putchar('\n');
+        }
+}
