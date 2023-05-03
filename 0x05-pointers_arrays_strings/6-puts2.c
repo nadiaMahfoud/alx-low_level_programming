5e32faaed022db15714a7472580fbaab46812cbd
Nadia <nadiamahfoud48@gmail.com>
2023-05-02 17:00:58 -0700

    0x04-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,15 +2,24 @@
 
 /**
  * puts2 - prints one char out of 2 of a string
- * @str: input string
- *
- * Return: void
+ * followed by a new line
+ * @str: string to print the chars from
  */
 void puts2(char *str)
 {
-	int i;
+	int len, i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
+	{
 		_putchar(str[i]);
+	}
+
 	_putchar('\n');
 }
