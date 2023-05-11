ce1e2e6386643ab105a7d20662b201571ece14bc
Nadia <nadiamahfoud48@gmail.com>
2023-05-11 08:00:44 -0700

    0x08. c - recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,18 +9,18 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0') /* If both strings are empty */
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s2 == '*')
 	{
-		if (*s1 == '\0' && *(s2 + 1) == '\0') /* If the wildcard is the last character in s2 */
+		if (*s1 == '\0' && *(s2 + 1) == '\0')
 			return (1);
-		if (*s1 == '\0') /* If the wildcard isn't the last character in s2 and s1 is empty */
+		if (*s1 == '\0')
 			return (0);
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)) /* Recursively check with and without matching the wildcard */
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
 			return (1);
 	}
 	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1)); /* Recursively check the rest of the strings */
+		return (wildcmp(s1 + 1, s2 + 1));
 	return (0);
 }
