b9892d59e2a22213d2e2faaf28aa30bb6ff0dcda
Nadia <nadiamahfoud48@gmail.com>
2023-05-10 17:08:54 -0700

    0x08. c - recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,42 +1,30 @@
 #include "main.h"
 /**
- * is_prime_number - checks if a number is prime
- * @n: the number to check
+ * prime - Makes possible to evaluate from 1 to n
+ * @i: same number as n
+ * @j: number that iterates from 1 to n
  *
- * Return: 1 if n is prime, 0 otherwise
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int prime(int i, int j)
+{
+	if (i == j)
+		return (1);
+	else if (i % j == 0)
+		return (0);
+	return (prime(i, j + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int is_prime_number(int n)
 {
-	/* check for base cases */
-	if (n < 2)
+	if (n <= 1)
 		return (0);
-	if (n == 2 || n == 3)
-		return (1);
-
-	/* check if n is divisible by 2 or 3 */
-	if (n % 2 == 0 || n % 3 == 0)
-		return (0);
-
-	return (is_prime_helper(n, 5));
-}
-
-/**
- * is_prime_helper - helper function for is_prime_number
- * @n: the number to check
- * @i: the current divisor to check
- *
- * Return: 1 if n is prime, 0 otherwise
- */
-int is_prime_helper(int n, int i)
-{
-	/* check if i is greater than the square root of n */
-	if (i * i > n)
-		return (1);
-
-	/* check if n is divisible by i */
-	if (n % i == 0)
-		return (0);
-
-	/* try the next odd divisor */
-	return (is_prime_helper(n, i + 2));
+	return (prime(n, 2));
 }
