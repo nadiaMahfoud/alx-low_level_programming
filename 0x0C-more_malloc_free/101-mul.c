1cb575c5a39a264857c9ad5d2876961cabc5de2c
Nadia <nadiamahfoud48@gmail.com>
2023-05-18 12:48:49 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,39 +1,96 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
+ * validate_input - Validates if the given number is composed of digits.
+ * @num: The number to validate
  *
- * Return: 0 on success, 98 on error
+ * Return: 0 if the number is valid, 1 otherwise
  */
-int main(int argc, char *argv[])
+int validate_input(char *num)
 {
-	unsigned long mul;
-	int i, j;
+	int i;
 
-	if (argc != 3)
+	for (i = 0; num[i] != '\0'; i++)
 	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if (num[i] < '0' || num[i] > '9')
 		{
-			if (argv[i][j] > '9' || argv[i][j] < '0')
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			printf("Error\n");
+			return (1);
 		}
 	}
 
-	mul = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", mul);
+	return (0);
+}
+
+/**
+ * multiply_numbers - Multiplies two numbers and prints the result.
+ * @num1: The first number
+ * @num2: The second number
+ */
+void multiply_numbers(char *num1, char *num2)
+{
+	int len1, len2, len, i, j;
+	int *result;
+
+	len1 = 0;
+	while (num1[len1] != '\0')
+		len1++;
+
+	len2 = 0;
+	while (num2[len2] != '\0')
+		len2++;
+
+	len = len1 + len2;
+	result = calloc(len, sizeof(int));
+	if (result == NULL)
+	{
+		printf("Error\n");
+		return;
+	}
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			int mul = (num1[i] - '0') * (num2[j] - '0');
+			int sum = result[i + j + 1] + mul;
+
+			result[i + j] += sum / 10;
+			result[i + j + 1] = sum % 10;
+		}
+	}
+
+	i = 0;
+	while (result[i] == 0)
+		i++;
+
+	for (; i < len; i++)
+		printf("%d", result[i]);
+	printf("\n");
+
+	free(result);
+}
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (validate_input(argv[1]) || validate_input(argv[2]))
+		return (1);
+
+	multiply_numbers(argv[1], argv[2]);
 
 	return (0);
 }
