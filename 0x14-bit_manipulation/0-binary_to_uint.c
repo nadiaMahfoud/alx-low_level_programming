8c6a8908a17c676b355c9f44e616082e1b82e618
nadiaMahfoud <nadiamahfoud48@gmail.com>
2023-06-08 16:44:18 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,15 +10,15 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int decimal;
-	unsigned int j;
+	unsigned int i;
 
-	for (decimal = 0, j = 0; b[j] != '\0'; j++)
+	for (decimal = 0, i = 0; b[i] != '\0'; i++)
 	{
-		if (b[j] == '1')
+		if (b[i] == '1')
 			decimal = (decimal << 1) | 1;
-		else if (b[j] == '0')
+		else if (b[i] == '0')
 			decimal <<= 1;
-		else if (b[j] != '0' && b[j] != '1')
+		else if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
 
