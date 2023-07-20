532990e02b005c0ed8add0c0217089fbba31d29e
nadiMahfoud <nadiamahfoud48@gmail.com>
2023-07-20 12:50:52 -0700

    nm

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,7 @@
 /**
  * print_dlistint -function that prints all the elements of a list
  * @h: pointer to the list
- * Return: number of nodes
+ * Return: number of node
  */
 size_t print_dlistint(const dlistint_t *h)
 {
