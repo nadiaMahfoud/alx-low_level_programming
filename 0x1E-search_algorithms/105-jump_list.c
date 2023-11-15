e24f0d60eddd6deaa565ef410d236c342c85ca62
nadiaMahfoud <nadiamahfoud48@gmail.com>
2023-11-15 08:00:17 -0800

    last files

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,54 @@
+#include "search_algos.h"
+
+/**
+ * jump_list -to Search for an algorithm in a sorted singly
+ *             linked list of integers using jump search.
+ * @list: A pointer to the  head of the linked list to search.
+ * @size: The number of nodes in the list.
+ * @value: The value to search for.
+ *
+ * Return: If the value is not present or the head of the list is NULL, NULL.
+ *         else, a pointer to the first node where the value is located.
+ *
+ * Description:it Prints a value every time it is compared in the list.
+ *              Uses the square root of the list size as the jump step.
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t index, j, m;
+	listint_t *prev;
+
+	if (list == NULL || size == 0)
+		return (NULL);
+
+	m = (size_t)sqrt((double)size);
+	index = 0;
+	j = 0;
+
+	do {
+		prev = list;
+		j++;
+		index = j * m;
+
+		while (list->next && list->index < index)
+			list = list->next;
+
+		if (list->next == NULL && index != list->index)
+			index = list->index;
+
+		printf("Value checked at index [%d] = [%d]\n", (int)index, list->n);
+
+	} while (index < size && list->next && list->n < value);
+
+	printf("Value found between indexes ");
+	printf("[%d] and [%d]\n", (int)prev->index, (int)list->index);
+
+	for (; prev && prev->index <= list->index; prev = prev->next)
+	{
+		printf("Value checked at index [%d] = [%d]\n", (int)prev->index, prev->n);
+		if (prev->n == value)
+			return (prev);
+	}
+
+	return (NULL);
+}
